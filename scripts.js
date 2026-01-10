1d1d08b94b208ca7d44bf2f0be8a61acd4c31847
plehovlev2011-glitch <plehovlev2011@gmail.com>
2026-01-10 09:31:26 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 $(function(){
-    // ОРИГИНАЛЬНЫЙ КОД SOPHIE (не трогаем!)
+    // ОРИГИНАЛЬНЫЙ КОД SOPHIE
     setTimeout(function(){
         scrollTo(0,-1);
     },0);
@@ -10,202 +10,99 @@ $(function(){
     var down = 0;
     var timer = 0;
 
-    $( "html,body" ).keypress(function( event ) {
-      if ( event.which == 112 ) {
-        $("html,body").animate({"scrollTop":30000},100000,"linear")
-      }
-      })
-
     $("html,body").scrollTop(0);
-
-    /*
-
-    $(window).bind('beforeunload',function(){
-
-        $("html,body").scrollTop(0);
-
-    });
-    */
-
 
     $("img").width($(window).width());
 
-
-
     $(window).on("click",function(){
-        
         if($(window).scrollTop() == 0){
             $("html,body").animate({"scrollTop":$(".viewport").height()})
         }
     })
 
-
     function init(){
-
         $("img").width($(window).width());
-        // $(".viewport").height($(".viewport").height());
-        console.log("a");
-
     }
 
-
     $(window).on("resize",function(){
-        console.log("resize");
         init();
     });
 
-
-
     $(window).on("scroll",function(){
-
         var cur = $(window).scrollTop();
         
         //Image sizing
-
-        if(t == 0 ){
-
+        if(t == 0){
             var math = cur - $(".viewport").height()*(i-1);
             $("img").height(math);
-
         }else{
-
             var math = $(".viewport").height()*(i)- cur;
             $("img").height(math);
-
         }
 
         //trigger
-
         if(cur > $(".viewport").height()*i){
-
-                
             i++;
             togglePosition();
             $("html,body").height($("html,body").height()+$(".viewport").height())
-        
-
         }
 
         //scrolling up
-
         if(cur + 5 < $(".viewport").height()*i - ($(".viewport").height())){
-            
             i--;
             togglePosition();
-
-            down = 1
-
+            down = 1;
         }
 
         //reset 
-
         if(cur <= 0){
-
-            console.log("reset");
-            
             $(".page.a").removeClass("a");
             $(".page").eq(0).addClass("a");
         }
 
         //change text
-
-        
-
         if($("img").height() == $(".viewport").height() && cur > 10){
-            
             if(timer == 0){
-
                 timer = 1;
                 text();
                 setTimeout(function(){
                     timer = 0;
                 },300)
-
-
-
             }
-            
-
-            
-
         }
 
         //alternate images
-
         if($("img").height() <= 0){
-            
             if (imgt%2 == 0){
-
                 $("img").hide().eq(1).show();
-
             }else{
-
                 $("img").hide().eq(0).show();
-
             }
-
             imgt++;
-
         }
-
-        
-
-        $("#mc_embed_signup input").on("click",function(e){
-            
-            $(this).val("")
-            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-                $("img").hide();
-                $("ul#active").hide();
-            }
-
-            
-
-        });
-        
-        
-        $("input.email").on("focusout",function(e){
-            
-            $(this).val("Email Address")
-
-            
-
-        });
-
     });
 
-
     function togglePosition(){
-
-
         if (i%2 == 0){
-
             t = 0;
             $("img").css({"top":"auto","bottom":"0"});
-
         }else{
-
             t = 1;
             $("img").css({"top":"0","bottom":"auto"});
-        
         }
-
     }
 
-        
     function text(){
-
         if($(".page.a").next().length == 0){
             $(".page.a").removeClass("a");
             $(".page").eq(0).addClass("a");
-
         }else{
             $(".page.a").removeClass("a").next().addClass("a");
         }
-
     }
 
     // ================================================
-    // ДОБАВЛЕННЫЙ КОД ДЛЯ СЕКРЕТНОЙ СТРАНИЦЫ
+    // ДОБАВЛЕННЫЙ КОД ДЛЯ КНОПОК
     // ================================================
 
     // Инициализация аудио
@@ -229,7 +126,7 @@ $(function(){
         }, 150);
     }
     
-    // Переход на секретную страницу
+    // Переход на страницу ME
     $('#goToSecret').on('click', function(e) {
         e.preventDefault();
         e.stopPropagation();
@@ -237,38 +134,14 @@ $(function(){
         playClickSound();
         animateClick(this);
         
-        // Скрываем основную страницу, показываем секретную
+        // Переход через 200ms (для звука)
         setTimeout(function() {
-            $('.viewport').hide();
-            $('.secret-page').show();
-            // Останавливаем скролл эффект
-            $(window).off('scroll');
+            window.location.href = 'me.html';
         }, 200);
     });
     
-    // Возврат с секретной страницы
-    $('#goBack').on('click', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        
-        playClickSound();
-        animateClick(this);
-        
-        // Возвращаемся на основную страницу
-        setTimeout(function() {
-            $('.secret-page').hide();
-            $('.viewport').show();
-            // Восстанавливаем скролл эффект
-            $(window).on('scroll', arguments.callee);
-            // Сбрасываем скролл
-            $("html,body").scrollTop(0);
-            i = 1;
-            t = 1;
-        }, 200);
-    });
-    
-    // Обработчики для кнопок с ссылками (кроме ME и BACK)
-    $('.styled-btn:not(#goToSecret):not(#goBack)').on('click', function(e) {
+    // Обработчики для кнопок с ссылками (кроме ME)
+    $('.styled-btn:not(#goToSecret)').on('click', function(e) {
         e.preventDefault();
         e.stopPropagation();
         
